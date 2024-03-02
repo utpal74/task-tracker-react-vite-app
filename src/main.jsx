3371764d3b59e6a26a90469836ad30ac2672fa31
UTPAL KUMAR <utpal.kumar74@gmail.com>
2024-03-02 23:37:05 +0530

    Adding google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGa from "react-ga";
+import reportWebVitals from "./reportWebVitals"
+
+const TRACKING_ID = "G-SXL6T8M54Y";
+ReactGa.initialize(TRACKING_ID);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+const SendAnalytics = ()=> {
+  ReactGa.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
+reportWebVitals(SendAnalytics);
